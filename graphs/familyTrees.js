38dcd6d97596b924bc7d52b12ca627126b78fc14
Bianca Pelegrini <bmpelegrini@gmail.com>
2023-06-16 19:03:57 -0300

    family trees works i guess?

--- graphs/familyTrees.js
+++ graphs/familyTrees.js
@@ -1,41 +1,67 @@
-const input = `8 8
-Pedro marido Maria
-Pedro pai Josias
-Josias irmao Mangojata
-Maria mae Mangojata
-Samuel filho Maria
-Paulo filho Marcos
-Samuel tio Ivane
-Mangojata mae Ivane`;
+const input = `9 6
+Jose_1 marido Maria
+Josias marido Liboria
+Liboria mae Guapo
+Sandra filho Maria
+Paulo filho Jose_2
+Sandra mae Ivanir`;
 const lines = input.split("\n");
 
-function main() {
-  let [numPeople, _] = lines.shift().split(" ");
+let relations = [];
 
-  // init people graph
-  let relations = new Map();
+function main() {
+  lines.shift();
+
+  relations = [];
 
   for (const line of lines) {
     let [firstPerson, relation, secondPerson] = line.split(" ");
-    let firstPersonRelations =
-      relations.get(firstPerson) === undefined
-        ? []
-        : relations.get(firstPerson);
-    firstPersonRelations.push(secondPerson);
-    relations.set(firstPerson, firstPersonRelations);
 
-    let secondPersonRelations =
-      relations.get(secondPerson) === undefined
-        ? []
-        : relations.get(secondPerson);
-    secondPersonRelations.push(firstPerson);
-    relations.set(secondPerson, secondPersonRelations);
+    let firstIdx = relations.findIndex((r) => r.name === firstPerson);
+    let firstRelatives =
+      firstIdx === -1 ? [] : [...relations[firstIdx].relatives];
+    firstRelatives.push(secondPerson);
+
+    let firstObject = {
+      name: firstPerson,
+      visited: false,
+      relatives: firstRelatives,
+    };
+    if (firstIdx === -1) relations.push(firstObject);
+    else relations[firstIdx] = firstObject;
+
+    let secondIdx = relations.findIndex((r) => r.name === secondPerson);
+    let secondRelatives =
+      secondIdx === -1 ? [] : [...relations[secondIdx].relatives];
+    secondRelatives.push(firstPerson);
+
+    let secondObject = {
+      name: secondPerson,
+      visited: false,
+      relatives: secondRelatives,
+    };
+    if (secondIdx === -1) relations.push(secondObject);
+    else relations[secondIdx] = secondObject;
   }
 
   let families = 0;
-  for (const [key, value] of relations) {
-    console.log(key, value);
+
+  for (let i = 0; i < relations.length; i++) {
+    if (!relations[i].visited) {
+      dfs(relations[i].name);
+      families++;
+    }
   }
+
+  console.log(families);
+}
+
+function dfs(personName /*: string */) {
+  let idx = relations.findIndex((rel) => rel.name === personName);
+  if (relations[idx].visited) return;
+
+  relations[idx] = { ...relations[idx], visited: true };
+  for (const relative of relations[idx].relatives) dfs(relative);
 }
 
 main();
